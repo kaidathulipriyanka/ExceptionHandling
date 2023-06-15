29a7e8387a4cb6de3ae205d9d76846f033199580
kaidathulipriyanka <kaidhathulipriyanka@gmail.com>
2023-06-15 12:38:41 +0530

    [Priyanka] Add: ExceptionHandling

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿namespace UsingException_UserRegistrationProblem
+{
+    public class Program  
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string firstName = GetValidFirstName();
+                string lastName = GetValidLastName();
+                string email = GetValidEmail();
+                string mobile = GetValidMobile();
+                string password = GetValidPassword();
+
+                Console.WriteLine("User Registration Successful!");
+                Console.WriteLine("First Name: " + firstName);
+                Console.WriteLine("Last Name: " + lastName);
+                Console.WriteLine("Email: " + email);
+                Console.WriteLine("Mobile: " + mobile);
+                Console.WriteLine("Password: " + password);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("User Registration Failed: " + ex.Message);
+            }
+        }
+
+        private static string GetValidPassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string GetValidMobile()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string GetValidEmail()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static string GetValidLastName()
+        {
+            throw new NotImplementedException();
+        } 
+
+        private static string GetValidFirstName()
+        {
+            throw new NotImplementedException();
+        }   
+    }
+    }
